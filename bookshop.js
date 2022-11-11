e85097ea9927d4f5bc0465bfffa1663dde062724
Jurand76 <jurand@interia.pl>
2022-11-11 17:37:48 +0100

    fix: menu improved

--- bookshop.js
+++ bookshop.js
@@ -1,10 +1,33 @@
 var wrapper = document.getElementById("wrapper");
 
-var menu = document.createElement("div");
-menu.id = "menu";
-wrapper.appendChild(menu);
+var menu_bar = document.createElement("div");
+menu_bar.id = "menu_bar";
+wrapper.appendChild(menu_bar);
 
 var logo = document.createElement("img");
 logo.src = "../icons/logo.png";
 logo.id = "logo";
-menu.appendChild(logo);
+menu_bar.appendChild(logo);
+
+var menu = document.createElement("div");
+menu.id = "menu";
+menu_bar.appendChild(menu);
+
+var menu_item1 = document.createElement("span");
+menu_item1.textContent = "STORE";
+menu_item1.className = "menu_item";
+menu.appendChild(menu_item1);
+
+var menu_item2 = document.createElement("span");
+menu_item2.textContent = "BLOG";
+menu_item2.className = "menu_item";
+menu.appendChild(menu_item2);
+
+var menu_item3 = document.createElement("span");
+menu_item3.textContent = "ABOUT US";
+menu_item3.className = "menu_item";
+menu.appendChild(menu_item3);
+
+var store_content = document.createElement("div");
+store_content.id = "store_content";
+wrapper.appendChild(store_content);
