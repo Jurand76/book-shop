fcf874eeacfe176a0ea76ad108eae136b13a5dab
Jurand76 <jurand@interia.pl>
2022-11-13 22:57:53 +0100

    fix: improve book catalog show

--- bookshop.js
+++ bookshop.js
@@ -1,60 +1,80 @@
-var books = fetch("./books.json").then((response) => null);
-console.log(books);
+async function getBooks() {
+  const response = await fetch(filename);
+  const result = await response.json();
+  return result;
+}
 
-var wrapper = document.getElementById("wrapper");
+async function getBooks2() {
+  const bookbase = await getBooks();
+  for (let i = 0; i < bookbase.length; i++) books.push(bookbase[i]);
+}
 
-var menu_bar = document.createElement("div");
-menu_bar.id = "menu_bar";
-wrapper.appendChild(menu_bar);
+async function DisplayStore() {
+  console.log("Book catalog : ", books);
 
-var logo = document.createElement("img");
-logo.src = "./icons/logo2.png";
-logo.id = "logo";
-menu_bar.appendChild(logo);
+  var wrapper = document.getElementById("wrapper");
+  var menu_bar = document.createElement("div");
+  menu_bar.id = "menu_bar";
+  wrapper.appendChild(menu_bar);
 
-var menu = document.createElement("div");
-menu.id = "menu";
-menu_bar.appendChild(menu);
+  var logo = document.createElement("img");
+  logo.src = "./icons/logo2.png";
+  logo.id = "logo";
+  menu_bar.appendChild(logo);
 
-var menu_item1 = document.createElement("span");
-menu_item1.textContent = "STORE";
-menu_item1.className = "menu_item";
-menu.appendChild(menu_item1);
+  var menu = document.createElement("div");
+  menu.id = "menu";
+  menu_bar.appendChild(menu);
 
-var menu_item2 = document.createElement("span");
-menu_item2.textContent = "BLOG";
-menu_item2.className = "menu_item";
-menu.appendChild(menu_item2);
+  var menu_item1 = document.createElement("span");
+  menu_item1.textContent = "STORE";
+  menu_item1.className = "menu_item";
+  menu.appendChild(menu_item1);
 
-var menu_item3 = document.createElement("span");
-menu_item3.textContent = "ABOUT US";
-menu_item3.className = "menu_item";
-menu.appendChild(menu_item3);
+  var menu_item2 = document.createElement("span");
+  menu_item2.textContent = "BLOG";
+  menu_item2.className = "menu_item";
+  menu.appendChild(menu_item2);
 
-var store_content = document.createElement("div");
-store_content.id = "store_content";
-wrapper.appendChild(store_content);
+  var menu_item3 = document.createElement("span");
+  menu_item3.textContent = "ABOUT US";
+  menu_item3.className = "menu_item";
+  menu.appendChild(menu_item3);
 
-var grid_books = document.createElement("grid");
-grid_books.id = "grid_books";
-store_content.appendChild(grid_books);
+  var store_content = document.createElement("div");
+  store_content.id = "store_content";
+  wrapper.appendChild(store_content);
 
-var book_desc = document.createElement("div");
-book_desc.className = "book1 book_entry";
-book_desc.textContent = "Book 1";
-grid_books.appendChild(book_desc);
+  var grid_books = document.createElement("grid");
+  grid_books.id = "grid_books";
+  store_content.appendChild(grid_books);
 
-book_desc = document.createElement("div");
-book_desc.className = "book2 book_entry";
-book_desc.textContent = "Book 2";
-grid_books.appendChild(book_desc);
+  for (let i = 1; i < 5; i++) {
+    book_desc = document.createElement("div");
+    book_desc.className = "book" + i + " book_entry";
+    grid_books.appendChild(book_desc);
 
-book_desc = document.createElement("div");
-book_desc.className = "book3 book_entry";
-book_desc.textContent = "Book 3";
-grid_books.appendChild(book_desc);
+    book_author = document.createElement("span");
+    book_author.textContent = books[bookStart + i - 1].author;
+    book_author.className = "book_author";
+    book_desc.appendChild(book_author);
 
-book_desc = document.createElement("div");
-book_desc.className = "book4 book_entry";
-book_desc.textContent = "Book 4";
-grid_books.appendChild(book_desc);
+    book_title = document.createElement("span");
+    book_title.className = "book_title";
+    book_title.textContent = books[bookStart + i - 1].title;
+    book_desc.appendChild(book_title);
+  }
+}
+
+async function MainDisplay() {
+  await getBooks2();
+  if (displayscreen == 1) DisplayStore();
+  if (displayscreen == 2) DisplayBlog();
+  if (displayscreen == 3) DisplayAboutUs();
+}
+
+const filename = "./books.json";
+var displayscreen = 1;
+var bookStart = 0;
+var books = Array();
+MainDisplay();
